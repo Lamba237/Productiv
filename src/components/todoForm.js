72806b260c0155934ce0807d531da04dadbce127
Lamba237 <simontanyi2004@gmail.com>
2025-01-09 11:56:31 +0100

    Made it in a way  that our form is cleared when entered

--- src/components/todoForm.js
+++ src/components/todoForm.js
@@ -12,12 +12,13 @@ export const TodoForm = ({addTodo}) => {
         // and reset the input field
         // After this, we will clear the input field
         addTodo(task);
+        setTask("");
     }
 
     return (
     // We will then add a function below that captures the input value
     <form className='TodoForm' onSubmit={handleSubmit}>
-        <input type='text' className='todo-input' placeholder='What is the task for today ?' onChange={(event) => setTask(event.target.task)}/>
+        <input type='text' className='todo-input' value={task} placeholder='What is the task for today ?' onChange={(event) => setTask(event.target.task)}/>
         <button type='submit' className='todo-btn'>Add task</button>
     </form>
   )
